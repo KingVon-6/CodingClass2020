fc0158757ecb35498f82a28549fda9e07de16244
Devon Donaldson <Devondonaldson13@gmail.com>
2020-09-22 12:32:26 -0500

    assignment #2

--- Inclass assignment2/sketch.js
+++ Inclass assignment2/sketch.js
@@ -1,38 +1,49 @@
-let xpos = 200;
-let ypos = 300;
-let ballspeed = 1;
+let xpos = 500;
+let ypos = 0;
+let xballspeed = 10;
+let yballspeed = 15;
+let cirlcesize = 10;
 
  function setup() {
-
+    //put setup code here
   createCanvas(windowWidth,windowHeight);
-
+  circlesize = random(10,100);
 }
 function draw() {
-
-  background(200,10,120)
+     // put draw code here
+  background(0,120,0)
   fill(0,0,200);
+
+  if (dist(xpos,ypos,mouseX,mouseY) < 500) {
+
+  }
+
+  
   circle(xpos,ypos,100);
 
-if(keyIsPressed & keycode === Up_Arrow) {
-     ypos = ypos + ballspeed;
-}
+   xpos = xpos + xballspeed;
+   ypos = ypos + yballspeed;
 
-if(keyIsPressed & keycode === Down_Arrow){
-     xpos = xpos + ballspeed;
-}
-
-if(keyIsPressed & keycode === Left_Arrow){
-     xpos = xpos - ballspeed;
-}
-if(keyIsPressed & keycode === Right_Arrow){
-     xpos = xpos - ballspeed;
-}
+   // check for x position
+   // check when ball cross right side of the canvas
+   if(xpos > width){
+     xballspeed = -xballspeed;
+   }
+   //check when ball cross left side of the canvas
+   if(xpos < 0){
+     xballspeed = -xballspeed;
+   }
 
 
 
+   // check for y position
 
-
-
-
-
+   //check when ball crosses the botrom of the Canvas
+   if(ypos > height){
+     yballspeed = -yballspeed;
+   }
+   //check when ball crosses top of the canvas
+   if(ypos < 0){
+     yballspeed = -yballspeed;
+   }
 }
